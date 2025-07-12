1e305ef923113ea231950bd33be208b1e507d5e8
bmonagan <bmonagan@gmail.com>
2025-07-12 02:45:11 -0400

    Finished resizing images and added some more reminders to the function

--- image_resizer.py
+++ image_resizer.py
@@ -6,8 +6,12 @@ input_folder = 'goodreads data/goodreads_book_covers'
 output_folder = 'goodreads data/goodreads_covers_resized'
 size = (512, 512)
 
+
+print(f"Resizing images from {input_folder} to {output_folder}...")
 for filename in os.listdir(input_folder):
     if filename.endswith('.jpg') and filename not in os.listdir(output_folder):
         img = Image.open(os.path.join(input_folder, filename))
         img.thumbnail(size)
         img.save(os.path.join(output_folder, filename))
+        print(f"Resized and saved: {filename}")
+print("Resizing complete.")
