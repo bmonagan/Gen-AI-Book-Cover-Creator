5cca8c39b08d401b6155ab589dc350be81ab9f72
bmonagan <bmonagan@gmail.com>
2025-07-13 02:41:24 -0400

    Pulled most of the trainingloop from huggingface with some adjustments to fit the changes i needed in training. Running epochs at this point.

--- src/training/train.py
+++ src/training/train.py
@@ -1,12 +1,22 @@
+# Standard library
+import os
+from pathlib import Path
 
-import torch
-from datasets import load_dataset
+# Third-party libraries
 import matplotlib.pyplot as plt
-from diffusers import UNet2DModel, DDPMScheduler
-from PIL import Image
-from torchvision import transforms
+import torch
 import torch.nn.functional as F
+from PIL import Image
+from tqdm.auto import tqdm
+from torchvision import transforms
+
+# Hugging Face libraries
+from accelerate import Accelerator
+from datasets import load_dataset
+from diffusers import UNet2DModel, DDPMScheduler
 from diffusers.optimization import get_cosine_schedule_with_warmup
+from diffusers.utils import make_image_grid
+from huggingface_hub import create_repo, upload_folder
 
 # Import local training configuration
 import training_config
@@ -14,13 +24,18 @@ import training_config
 # Load the training configuration
 config = training_config.TrainingConfig()
 
+# Create a directory for the trained model
+model_dir = Path("goodreads_model")
+model_dir.mkdir(exist_ok=True)
+
+
 
 # Load the local dataset
 # Images have been preprocessed and resized to 512x512 pixels(which will change to 128x128 later)
 # Also ensured that the images are in RGB format
 dataset = load_dataset(
     "imagefolder",
-    data_dir="goodreads data/goodreads_covers_resized"
+    data_dir="data/goodreads data/goodreads_covers_resized"
 )
 # Further processing the dataset
 # Convert images to tensors and normalize them
@@ -39,7 +54,7 @@ def transform(examples):
 
 
 dataset.set_transform(transform)
-train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
+train_dataloader = torch.utils.data.DataLoader(dataset["train"], batch_size=config.train_batch_size, shuffle=True)
 
 # Common diffusers UNet2DModel configuration
 # This is a simplified version of the UNet2DModel configuration
@@ -85,3 +100,113 @@ lr_scheduler = get_cosine_schedule_with_warmup(
     num_warmup_steps=config.lr_warmup_steps,
     num_training_steps=(len(train_dataloader) * config.num_epochs),
 )
+
+
+
+
+def evaluate(config, epoch, pipeline):
+    # Sample some images from random noise (this is the backward diffusion process).
+    # The default pipeline output type is `List[PIL.Image]`
+    images = pipeline(
+        batch_size=config.eval_batch_size,
+        generator=torch.Generator(device='cpu').manual_seed(config.seed), # Use a separate torch generator to avoid rewinding the random state of the main training loop
+    ).images
+
+    # Make a grid out of the images
+    image_grid = make_image_grid(images, rows=4, cols=4)
+
+    # Save the images
+    test_dir = os.path.join(config.output_dir, "samples")
+    os.makedirs(test_dir, exist_ok=True)
+    image_grid.save(f"{test_dir}/{epoch:04d}.png")
+
+
+def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
+    # Initialize accelerator and tensorboard logging
+    accelerator = Accelerator(
+        mixed_precision=config.mixed_precision,
+        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        log_with="tensorboard",
+        project_dir=os.path.join(config.output_dir, "logs"),
+    )
+    if accelerator.is_main_process:
+        if config.output_dir is not None:
+            os.makedirs(config.output_dir, exist_ok=True)
+        if config.push_to_hub:
+            repo_id = create_repo(
+                repo_id=config.hub_model_id or Path(config.output_dir).name, exist_ok=True
+            ).repo_id
+        accelerator.init_trackers("train_example")
+
+    # Prepare everything
+    # There is no specific order to remember, you just need to unpack the
+    # objects in the same order you gave them to the prepare method.
+    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_dataloader, lr_scheduler
+    )
+
+    global_step = 0
+
+    # Now you train the model
+    for epoch in range(config.num_epochs):
+        progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
+        progress_bar.set_description(f"Epoch {epoch}")
+
+        for step, batch in enumerate(train_dataloader):
+            clean_images = batch["images"]
+            # Sample noise to add to the images
+            noise = torch.randn(clean_images.shape, device=clean_images.device)
+            bs = clean_images.shape[0]
+
+            # Sample a random timestep for each image
+            timesteps = torch.randint(
+                0, noise_scheduler.config.num_train_timesteps, (bs,), device=clean_images.device,
+                dtype=torch.int64
+            )
+
+            # Add noise to the clean images according to the noise magnitude at each timestep
+            # (this is the forward diffusion process)
+            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+
+            with accelerator.accumulate(model):
+                # Predict the noise residual
+                noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+                loss = F.mse_loss(noise_pred, noise)
+                accelerator.backward(loss)
+
+                if accelerator.sync_gradients:
+                    accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+
+            progress_bar.update(1)
+            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
+            progress_bar.set_postfix(**logs)
+            accelerator.log(logs, step=global_step)
+            global_step += 1
+
+        # After each epoch you optionally sample some demo images with evaluate() and save the model
+        if accelerator.is_main_process:
+            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
+
+            if (epoch + 1) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
+                evaluate(config, epoch, pipeline)
+
+            if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
+                if config.push_to_hub:
+                    upload_folder(
+                        repo_id=repo_id,
+                        folder_path=config.output_dir,
+                        commit_message=f"Epoch {epoch}",
+                        ignore_patterns=["step_*", "epoch_*"],
+                    )
+                else:
+                    pipeline.save_pretrained(config.output_dir)
+
+
+from accelerate import notebook_launcher
+
+args = (config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+
+notebook_launcher(train_loop, args, num_processes=1)
--- src/training/training_config.py
+++ src/training/training_config.py
@@ -17,10 +17,10 @@ class TrainingConfig:
 
     #--- Optional parameters for uploading to the Hugging Face Hub ---#
     
-    # push_to_hub = True  # whether to upload the saved model to the HF Hub
-    # hub_model_id = "<your-username>/<my-awesome-model>"  # the name of the repository to create on the HF Hub
-    # hub_private_repo = None
-    # overwrite_output_dir = True  # overwrite the old model when re-running the notebook
-    # seed = 0
+    push_to_hub = False  # whether to upload the saved model to the HF Hub
+    hub_model_id = "<your-username>/<my-awesome-model>"  # the name of the repository to create on the HF Hub
+    hub_private_repo = None
+    overwrite_output_dir = False  # overwrite the old model when re-running the notebook
+    seed = 0
 
 
