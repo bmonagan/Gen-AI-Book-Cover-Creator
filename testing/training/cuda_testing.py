173105f445334d21ee441adac5f2558745a07907
bernard monagan <bernardmonagan@gmail.com>
2025-08-13 02:03:57 -0400

    Changes to cuda testing. Seems like the error had somethiing to do with UV

--- testing/training/cuda_testing.py
+++ testing/training/cuda_testing.py
@@ -1,7 +1,8 @@
 import torch
 print("CUDA Available: ", torch.cuda.is_available())
 print("Number of CUDA Devices:", torch.cuda.device_count())
+print(torch.version.cuda)
 
 # output 
 # CUDA Available:  False
-#Number of CUDA Devices: 0
+#Number of CUDA Devices: 
