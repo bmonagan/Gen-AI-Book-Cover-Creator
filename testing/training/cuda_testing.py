bb3f9f5988f353b08250ccddd5e7cad283516cb3
bernard monagan <bernardmonagan@gmail.com>
2025-08-18 13:45:54 -0400

    cuda has been tested and is available.

--- testing/training/cuda_testing.py
+++ testing/training/cuda_testing.py
@@ -3,6 +3,3 @@ print("CUDA Available: ", torch.cuda.is_available())
 print("Number of CUDA Devices:", torch.cuda.device_count())
 print(torch.version.cuda)
 
-# output 
-# CUDA Available:  False
-#Number of CUDA Devices: 
